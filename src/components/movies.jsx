867f6a8f1d66eb9bd8e0d869d31dffe7f5201f8c
Elissa Matsushita <elissamatsushita@Elissas-MacBook.local>
2020-04-17 16:31:20 -0700

    implement like button

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './like';
 
 class Movie extends Component {
   state = { 
@@ -8,6 +9,14 @@ class Movie extends Component {
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(moviesItem => moviesItem._id !== movie._id)
+    this.setState({movies})
+  }
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = {...movies[index]};
+    movies[index].liked = !movies[index].liked;
     this.setState({movies})
   }
 
@@ -31,6 +40,7 @@ class Movie extends Component {
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
             <th scope="col"></th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
@@ -39,6 +49,7 @@ class Movie extends Component {
               <th scope="row">{movie.title}</th>
               <td>{movie.genre.name}</td><td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
               <td>
                 <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
               </td>
