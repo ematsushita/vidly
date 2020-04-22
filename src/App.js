41d70896e10c04c353eaf41452f84656a7cc5d1b
Elissa Matsushita <elissamatsushita@Elissas-MacBook.local>
2020-04-22 11:53:55 -0700

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
 import Movie from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import MovieForm from './components/movieForm';
+import { Route, Redirect, Switch } from "react-router-dom";
+import NavBar from './components/navbar';
+
 
 
 function App() {
   return (
-    <main className="container">
-      <Movie />
-    </main>
+    <>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={ MovieForm }></Route>
+          <Route path="/movies" component={ Movie }></Route>
+          <Route path="/customers" component={ Customers }></Route>
+          <Route path="/rentals" component={ Rentals }></Route>
+          <Route path="/not-found" component={ NotFound }></Route>
+          <Redirect from="/" exact to="/movies"></Redirect>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+      </main>
+    </>
   );
 }
 
